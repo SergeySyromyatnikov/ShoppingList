088be98de6609a6a933544c7b5ade0de31308902
operator1 <syromyatnikovsv1@gmail.com>
2021-09-10 15:50:49 +0300

    Presentation layer edit

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -9,6 +9,7 @@ import com.example.shoppinglist.R
 class MainActivity : AppCompatActivity() {
 
     private  lateinit var viewModel: MainViewModel
+    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +17,12 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.shopList.observe(this){
             Log.d("MainActivityTest", it.toString())
+            if (count == 0){
+                count++
+                val item = it[0]
+                viewModel.deleteShopItem(item)
+            }
+
         }
-        viewModel.getShopList()
     }
 }
